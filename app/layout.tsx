6521cbc453751958340aabcf21f416064aad43af
varelaprogramador <varelaryan278@gmail.com>
2025-01-14 11:50:27 -0300

    update end

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Sora } from 'next/font/google'
 
 import { Toaster } from '@/components/ui/sonner'
 import { QueryProvider } from '@/providers/query-provider'
+import dynamic from 'next/dynamic'
 
 export const metadata: Metadata = {
   title: 'Captação | SpotForm',
@@ -24,6 +25,12 @@ export const metadata: Metadata = {
 
 const sora = Sora({ subsets: ['latin'] })
 
+// Dynamically import the Facebook Pixel script
+const FacebookPixelScript = dynamic(
+  () => import('@/components/FacebookPixel'),
+  { ssr: false }, // Disable server-side rendering for this component
+)
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -31,28 +38,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning className={sora.className}>
-      {process.env.FACEBOOK_PIXEL_ID &&
-        process.env.NODE_ENV === 'production' && (
-          <Script id="fb-pixel" strategy="afterInteractive">
-            {`
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${process.env.FACEBOOK_PIXEL_ID}');
-              fbq('track', 'PageView');
-            `}
-          </Script>
-        )}
-
       <body className="flex size-full min-h-dvh flex-col overflow-x-hidden antialiased">
         <QueryProvider>
           <Toaster richColors closeButton position="top-center" />
-
+          <FacebookPixelScript />{' '}
+          {/* This will be rendered only on the client side */}
           {children}
         </QueryProvider>
       </body>
